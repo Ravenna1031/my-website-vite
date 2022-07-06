c33c0715a42ebc6199a159b1e8d2b5ebe4088fa8
Ravenna1031 <cb5641@qq.com>
2022-07-06 09:18:57 +0800

    update homepage info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,13 @@
     </div>
     <div class="content">
       <p>
-        Hi, I am Ray Gu, a software development engineer interested in creative work.
-        <br>
+        Hi, I am Ray Gu, a software engineer interested in creative work.
+      </p>
+      <p>
         Trying new things and to learn about them is a great passion of mine, and the combination of art and program
         really fascinated me. You can find my work
         <span>
-          <router-link :to="{ name: 'gallery' }"> in the Gallery</router-link> <span /> .
+          <router-link :to="{ name: 'gallery' }"> in the Gallery</router-link>.
         </span>
       </p>
     </div>
